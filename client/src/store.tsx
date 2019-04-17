65ff3644112bfaa1ff8bed96a49ed9828aa7135e
Rajae Chetouani <rchetouani@sqli.com>
2019-04-17 14:36:06 +0100

    mern-auth-ts

---
+++ client/src/store.tsx
@@ -0,0 +1,14 @@
+import { applyMiddleware, createStore } from "redux";
+
+import rootReducer from "./reducers";
+
+import thunk from "redux-thunk";
+
+const w: any = window as any;
+const devtools: any = w.devToolsExtension
+  ? w.devToolsExtension()
+  : (f: any) => f;
+const middleware = applyMiddleware(thunk);
+const store: any = middleware(devtools(createStore))(rootReducer);
+
+export default store;
